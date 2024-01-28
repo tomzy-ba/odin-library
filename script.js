b48aec88c044e94cc7ec4545100617e3a3f8d26e
tomzy-ba <trbee100@gmail.com>
2024-01-28 10:16:13 +0000

    added js references

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-
+const bookDiv = document.querySelector("#book-div");
 const myLibrary = [];
 
+// function constructor
 function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
 }
 
+// this sends new books to the function constructor
 function addBookToLibrary() {
     const newBook = new Book(title, author, pages);
     myLibrary.push(newBook);
 }
 
 myLibrary.forEach(book => {
-    alert()
+    console.log(this)
 })
-
