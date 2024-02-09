666aac8aace4068d5fa58cc6c19ad35a41baf791
tomzy-ba <trbee100@gmail.com>
2024-02-09 20:10:55 +0000

    Add new book dialog and functionality

--- script.js
+++ script.js
@@ -6,14 +6,28 @@ class Book {
         this.author = author;
         this.pages = pages;
         this.read = false;
+        myLibrary.push(this)
     }
     toggleRead() {
         this.read = !this.read;
     }
 }
-const jeff = new Book("life of pi", "jeff", "321")
+new Book("life of pi", "jeff", "321")
 
-const newBookBtn = document.querySelector("#newBookButton");
-newBookBtn.addEventListener("click", () => {
-    new Book(prompt("Would you like"))
+
+document.querySelector("#newBookButton").addEventListener("click", () => {
+    const bookDialog = document.querySelector("#bookDialog");
+    const dialogSubmit = document.querySelector("#dialogSubmit");
+
+    const inputTitle = document.querySelector("#inputTitle");
+    const inputAuthor = document.querySelector("#inputAuthor");
+    const inputPages = document.querySelector("#inputPages");
+
+    bookDialog.showModal();
+    dialogSubmit.addEventListener("click", (event) => {
+        event.preventDefault();
+        new Book(inputTitle.value, inputAuthor.value, inputPages.value);
+
+    })
+
 })
